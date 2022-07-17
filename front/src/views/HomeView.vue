ca2450afa1d3dee53a44263389ccb074793b938f
tony <chcjswo@gmail.com>
2022-07-17 22:46:30 +0900

    리스트 페이지 적용

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const posts = ref([]);
+
+axios.get("http//localhost:8080/posts?page=1&size=5")
+.then(response => {
+  response.data.forEach((item: any) => {
+    posts.value.push(item);
+  })
+});
 </script>
 
 <template>
-  <main>
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>
+        {{ post.title }}
+      </div>
+
+      <div>
+        {{ post.content }}
+      </div>
+    </li>
+  </ul>
 </template>
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const title = ref("");
 const content = ref("");
 
+const router = useRouter();
+
 const write = () => {
-  axios.post("/api/posts", {
+  axios.post("http//localhost:8080/posts", {
     title: title.value,
     content: content.value
+  })
+  .then(() => {
+    router.replace({name: "home"});
   });
 };
 </script>
