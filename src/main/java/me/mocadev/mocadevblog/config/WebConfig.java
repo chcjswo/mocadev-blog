153752b6b8f319c944396a401a43326948a18674
tony <chcjswo@gmail.com>
2022-07-17 22:26:13 +0900

    vite.config.ts 적용 - cors 프론트측에서 해결

--- src/main/java/me/mocadev/mocadevblog/config/WebConfig.java
+++ src/main/java/me/mocadev/mocadevblog/config/WebConfig.java
@@ -1,7 +1,6 @@
 package me.mocadev.mocadevblog.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,9 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000");
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//			.allowedOrigins("http://localhost:3000");
+//	}
 }
