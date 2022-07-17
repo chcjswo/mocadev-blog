df152ab41874202b79fe63b6ef43a0a65a2f5283
tony <chcjswo@gmail.com>
2022-07-17 13:44:07 +0900

    CORS 서버측에서 해결 적용

---
+++ src/main/java/me/mocadev/mocadevblog/config/WebConfig.java
@@ -0,0 +1,22 @@
+package me.mocadev.mocadevblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2022-07-17
+ **/
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000");
+	}
+}
