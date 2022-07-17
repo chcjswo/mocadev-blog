2359e9ebc0f925dadd8880b05c35937873b035e0
tony <chcjswo@gmail.com>
2022-07-17 13:39:55 +0900

    글 작성 적용 및 불필요 vue.js 코드 정리

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,18 @@
 import {createApp} from "vue";
-import {createPinia} from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import "bootstrap/dist/css/bootstrap-utilities.min.css"
+
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(ElementPlus);
 app.use(router);
 
 app.mount("#app");
---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+
+const title = ref("");
+const content = ref("");
+
+const write = () => {
+  axios.post("http://localhost:8080/posts", {
+    title: title.value,
+    content: content.value
+  });
+};
+</script>
+
+<template>
+  <div>
+    <div class="mt-2">
+      <el-input placeholder="제목을 입력해주세요." v-model="title" />
+    </div>
+    <div class="mt-2">
+      <el-input
+        v-model="content"
+        :rows="15"
+        type="textarea"
+        placeholder="내용 입력"
+      />
+    </div>
+    <div class="mt-2">
+      <el-button type="primary" @click="write()">글 작성</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
